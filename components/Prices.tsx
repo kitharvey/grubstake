49f7c5b03c29580fd6a2241c29c08155333c176c
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2021-02-23 15:59:46 +0800

    hover translate to .5

--- components/Prices.tsx
+++ components/Prices.tsx
@@ -24,7 +24,7 @@ const Prices: React.FC<PricesComponentProps> = ({title}) => {
                         return (
                             <div 
                                 key={item.symbol}
-                                className='grid grid-cols-3 text-sm py-4 px-2 bg-white mt-2 rounded-md cursor-pointer transform transition-all hover:-translate-y-1 shadow hover:shadow-lg hover:text-blue-800' 
+                                className='grid grid-cols-3 text-sm py-4 px-2 bg-white mt-2 rounded-md cursor-pointer transform transition-all hover:-translate-y-0.5 shadow hover:shadow-lg hover:text-blue-800' 
                                 onClick={() => router.push({
                                     pathname: `/${title}/${item.symbol}`,
                                   })}
