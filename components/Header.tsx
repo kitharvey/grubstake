d2cc52949f0bdbb6bb42852e16f1f7cd41b1cd00
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2021-05-12 23:10:04 +0800

    change button to Link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,16 @@
 import React from "react"
-import { useRouter } from "next/router"
 import { DiGithubBadge } from "react-icons/di"
+import Link from "next/link"
 
 const Header = () => {
-  const router = useRouter()
   return (
     <div className="relative z-10">
       <div className="w-full h-auto bg-white">
         <div className="container mx-auto px-2 py-4 flex items-center justify-between">
           <div className="flex items-center">
-            <button type="button" onClick={() => router.push(`/`)}>
+            <Link href="/">
               <h1 className="text-3xl font-black cursor-pointer hover:text-blue-600">Shtonks</h1>
-            </button>
+            </Link>
 
             <a
               href="https://github.com/kitharvey/shtonks"
@@ -21,19 +20,6 @@ const Header = () => {
             >
               <DiGithubBadge />
             </a>
-          </div>
-
-          <div className="flex items-center">
-            {/* <label htmlFor="searchstock" className="text-black text-sm mr-3 " >Search: </label> */}
-            {/* <form method="GET" onSubmit={ (event: React.FormEvent<HTMLFormElement>) => event.preventDefault() }>
-                                                        <div className="flex text-gray-400 border-gray-200 border-solid border focus-within:text-black focus-within:border-gray-400 bg-white rounded w-80  p-2">
-                                                                <input id="searchstock" name="searchstock" className="w-full rounded text-xs font-thin focus:outline-none text-gray-900" placeholder="Search stock..." autoComplete="off"/>
-                                                                <button type='submit' className="border-0 outline-none focus:outline-none">
-                                                                        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" className="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                                                                </button>
-                                                                
-                                                        </div>
-                                                </form> */}
           </div>
         </div>
       </div>
--- components/PricesTickers.tsx
+++ components/PricesTickers.tsx
@@ -1,7 +1,6 @@
 import { FaAngleRight } from "react-icons/fa"
 import React from "react"
-// import majorIndexes from '../data/major';
-import { useRouter } from "next/router"
+import Link from "next/link"
 import { TickersPriceProps } from "../interfaces/interfaces"
 
 interface PricesComponentProps {
@@ -12,53 +11,42 @@ interface PricesComponentProps {
 
 const PricesTickers: React.FC<PricesComponentProps> = ({ title, priceData, link }) => {
   const items = priceData.slice(0, 5)
-  const router = useRouter()
   return (
     <div>
-      <button
-        type="button"
-        className="group flex items-center justify-between cursor-pointer hover:text-blue-800"
-        onClick={() =>
-          router.push({
-            pathname: `/${link}`,
-          })
-        }
-      >
-        <h1 className="text-lg font-black">{title}</h1>
-        <FaAngleRight />
-      </button>
+      <Link href={`/${link}`}>
+        <div className="group flex items-center justify-between cursor-pointer hover:text-blue-800">
+          <h1 className="text-lg font-black">{title}</h1>
+          <FaAngleRight />
+        </div>
+      </Link>
       <div>
         {items.map((item: TickersPriceProps) => {
           const changerate = item.changesPercentage.replace(/[()%]/g, "")
           return (
-            <button
-              type="button"
-              key={item.ticker}
-              className="grid grid-cols-3 text-sm py-4 px-2 bg-white mt-2 rounded-md cursor-pointer transform transition-all hover:-translate-y-0.5 shadow hover:shadow-lg hover:text-blue-800"
-              onClick={() =>
-                router.push({
-                  pathname: `/financial-summary/${item.ticker}`,
-                })
-              }
-            >
-              <div className="text-left">
-                <span>{item.ticker}</span>
+            <Link href={`/financial-summary/${item.ticker}`}>
+              <div
+                key={item.ticker}
+                className="grid grid-cols-3 text-sm py-4 px-2 bg-white mt-2 rounded-md cursor-pointer transform transition-all hover:-translate-y-0.5 shadow hover:shadow-lg hover:text-blue-800"
+              >
+                <div className="text-left">
+                  <span>{item.ticker}</span>
+                </div>
+                <div className="text-center">
+                  <span
+                    className={
+                      item.changes > 0
+                        ? "text-green-500 bg-green-100 px-2 py-1 rounded-full"
+                        : "text-red-500 bg-red-100 px-2 py-1 rounded-full"
+                    }
+                  >
+                    {+changerate}%
+                  </span>
+                </div>
+                <div className="text-right">
+                  <span>{item.price.toLocaleString()}</span>
+                </div>
               </div>
-              <div className="text-center">
-                <span
-                  className={
-                    item.changes > 0
-                      ? "text-green-500 bg-green-100 px-2 py-1 rounded-full"
-                      : "text-red-500 bg-red-100 px-2 py-1 rounded-full"
-                  }
-                >
-                  {+changerate}%
-                </span>
-              </div>
-              <div className="text-right">
-                <span>{item.price.toLocaleString()}</span>
-              </div>
-            </button>
+            </Link>
           )
         })}
       </div>
